18b47e9ab3f06ecc338dac3e1bac1cfe5008b2df
Caixuya <214089030@qq.com>
2020-12-17 09:04:46 +0800

    实训项目

---
+++ src/main/java/com/thxy/ch5_3/exception/MyException.java
@@ -0,0 +1,14 @@
+package com.thxy.ch5_3.exception;
+
+public class MyException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	public MyException() {
+	super();
+	}
+	public MyException(String message) {
+	super(message);
+	}
+
+	
+}
